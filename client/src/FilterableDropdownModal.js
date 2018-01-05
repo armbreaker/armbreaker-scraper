16fcb74521e458a61deae54994f2d5479fd8efad
Skyyrunner <fluffyterran@gmail.com>
2018-01-05 14:52:29 +0900

    Toggle in proper place. Still fiddling with align

--- client/src/FilterableDropdownModal.js
+++ client/src/FilterableDropdownModal.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import * as Popper from "popper.js";
+import Popper from "popper.js";
 
 export default class FilterableDropdownModal {
 	// Should be pairs of [value, text]
@@ -31,24 +31,63 @@ export default class FilterableDropdownModal {
 			return calculatedProperty(me, "line-height", name);
 		}
 
+		let removepx = str=>+str.slice(0, str.length - 2);
+
+		let calcModalWidth = function(me) {
+			let w    = calculatedProperty(me, "width", "modal");
+			let bw_r = calculatedProperty(me, "border-width").split(" ")[1];
+			return removepx(w) - removepx(bw_r) * 2;
+		}
+
 		let sel = d3.select(selector);
 		sel = sel
 			.append("div")
-			.classed("filterabledropdown-container", true);
-		sel
+			.classed("filterabledropdown-container filterabledropdown-width", true);
+		// selbox contains the dropdown toggle and the selected element
+		let selbox = sel
 			.append("div")
-			.classed("filterabledropdown-selected", true)
+			.classed("filterabledropdown-width", true);
+		// selected element
+		let valuebox = selbox
+			.append("div")
+			.classed("filterabledropdown-selected filterabledropdown-padded", true)
 			.style("height", function(){return calcHeight(this, "selected");})
 			.text(this.selected===null?" ":this.selected[1]);
-		sel = sel
+		let toggle = selbox
 			.append("div")
-			.classed("filterabledropdown-modal", true);
-		sel
+			.classed("filterabledropdown-toggle", true)
+			.text("▼")
+			.style("height", props["line-height-selected"])
+			.style("width" , props["line-height-selected"]);
+		let togglepopper = new Popper(selbox._groups[0][0], toggle._groups[0][0], {
+			placement: "right",
+			modifiers: {
+				flip: {
+					enabled: false
+				},
+				offset: {
+					offset: `0px,-${removepx(props["line-height-selected"])+1}px`
+				}
+			}
+		}); 
+		//// modal 
+		let modal = sel
+			.append("div")
+			.classed("filterabledropdown-modal filterabledropdown-width", true)
+		let popper = new Popper(selbox._groups[0][0], modal._groups[0][0], {
+			placement: "bottom",
+			modifiers: {
+				flip: {
+					enabled: false
+				}
+			}
+		});
+		modal
 			.append("input")
-			.classed("filterabledropdown-filterbox", true)
+			.classed("filterabledropdown-filterbox filterabledropdown-padded", true)
 			.attr("type", "text")
 			.style("height", function(){return calcHeight(this, "selected");});
-		sel
+		modal
 			.append("div")
 			.classed("filterabledropdown-options", true)
 			.on("keydown", function(){
@@ -77,5 +116,7 @@ export default class FilterableDropdownModal {
 				me.selected = d;
 				me.renderselected();
 			});
+		modal
+			.style("width", function(){return calcModalWidth(this)});
 	}
 }
