508cf3972ee021ce1bf4c354fb31e6590b040f43
NaGeL182 <nagel182@gmail.com>
2018-02-02 08:54:48 +0100

    temporary status

--- vagrantFiles/vagrantBootstrap.sh
+++ vagrantFiles/vagrantBootstrap.sh
@@ -1,19 +1,47 @@
 #!/usr/bin/env bash
+#variables
+DBPASSWRD="armbreaker"
 
+#adding PPA
 sudo add-apt-repository -y ppa:ondrej/php
 sudo add-apt-repository -y ppa:ondrej/apache2
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install -y apache2 php7.1 php-xdebug php7.1-zip php7.1-xml php7.1-mbstring git nodejs build-essential
+
+#debconf selection (the blue interactive stuffs)
+debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password password ${DBPASSWRD}"
+debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password_again password ${DBPASSWRD}"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password ${DBPASSWRD}"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password ${DBPASSWRD}"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password ${DBPASSWRD}"
+
+#installing debs
+sudo apt-get install -y apache2 \
+php7.1 php-xdebug php7.1-zip \
+php7.1-xml \
+php7.1-mbstring php7.1-mysql \
+git nodejs build-essential \
+mysql-server-5.7 phpmyadmin
+
+#setup apache2 server
 sudo a2enmod rewrite
 #need to do it like this becouse default cp is /bin/cp -i which forces interactivity
 /bin/cp -rf /vagrant/vagrantFiles/000-default.conf /etc/apache2/sites-available/000-default.conf
 /bin/cp -rf /vagrant/vagrantFiles/xdebug.ini /etc/php/7.1/mods-available/xdebug.ini
 sudo service apache2 restart
+
+#setup MySQL
+
+
+#setup webserver files
 if ! [ -L /var/www/html ]; then
     sudo rm -rf /var/www/html
     sudo ln -fs /vagrant/ /var/www/html
 fi
+
+#setup armbreaker
 sudo /vagrant/vagrantFiles/getComposer.sh
 cd /vagrant/
 ./update
