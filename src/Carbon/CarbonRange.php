7172db3a9575d38781b1106d64733bcf89881c8f
Keira Dueck <sylae@calref.net>
2019-06-08 01:00:48 -0700

    Refactor CarbonRange

--- src/Carbon/CarbonRange.php
+++ src/Carbon/CarbonRange.php
@@ -1,43 +1,44 @@
 <?php
 
-/*
- * Copyright (c) 2018 Armbreaker Developers.
+/**
+ * Copyright (c) 2019 Armbreaker Developers.
  * Use of this source code is governed by the MIT license, which
  * can be found in the LICENSE file.
  */
 
-namespace Armbreaker;
+namespace Carbon;
 
 /**
  * A less messy way to get date ranges.
  *
- * @author Keira Sylae Aro <sylae@calref.net>
+ * @author Keira Dueck <sylae@calref.net>
  */
 class CarbonRange
 {
 
     /**
-     * @var \Carbon\Carbon
+     * @var CarbonInterface
      */
     public $earliest = null;
 
     /**
-     * @var \Carbon\Carbon
+     * @var CarbonInterface
      */
     public $latest = null;
 
     /**
      * Parse a new date and recalc the min/max
-     * @param \Carbon\Carbon $new
+     *
+     * @param CarbonInterface $new
      */
-    public function addDate(\Carbon\Carbon $new)
+    public function addDate(CarbonInterface $new)
     {
         if (is_null($this->earliest) || is_null($this->latest)) {
             $this->earliest = clone $new;
-            $this->latest   = clone $new;
+            $this->latest = clone $new;
         } else {
             $this->earliest = clone $new->min($this->earliest);
-            $this->latest   = clone $new->max($this->latest);
+            $this->latest = clone $new->max($this->latest);
         }
     }
 
@@ -48,16 +49,16 @@ class CarbonRange
         }
         if (is_null($this->earliest) || is_null($this->latest)) {
             $this->earliest = clone $new->earliest;
-            $this->latest   = clone $new->latest;
+            $this->latest = clone $new->latest;
         } else {
             $this->earliest = clone $new->earliest->min($this->earliest);
-            $this->latest   = clone $new->latest->max($this->latest);
+            $this->latest = clone $new->latest->max($this->latest);
         }
     }
 
     public function atomEarliest(): ?string
     {
-        if ($this->earliest instanceof \Carbon\Carbon) {
+        if ($this->earliest instanceof CarbonInterface) {
             return $this->earliest->toAtomString();
         }
         return null;
@@ -65,7 +66,7 @@ class CarbonRange
 
     public function atomLatest(): ?string
     {
-        if ($this->latest instanceof \Carbon\Carbon) {
+        if ($this->latest instanceof CarbonInterface) {
             return $this->latest->toAtomString();
         }
         return null;
